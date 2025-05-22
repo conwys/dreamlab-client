ce85dc7d9dba5ef1ad1d66c764900ae4a2d9916f
Ben Lloyd <Ben.Lloyd@resonate.tech>
2025-05-22 16:58:25 +0100

    Finish styling for all pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { PlanComponent } from './features/plan/plan.component';
-import { AboutComponent } from './features/about/about.component';
 import { HomePageComponent } from './features/home-page/home-page.component';
 import { EditRoomComponent } from './features/edit-room/edit-room.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'plan', component: PlanComponent },
-  { path: 'about', component: AboutComponent },
   { path: 'edit', component: EditRoomComponent },
 ];
