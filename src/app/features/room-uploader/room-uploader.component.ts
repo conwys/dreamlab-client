8f7eea624dc8b00d7e988a95133d7440d7acfd06
Ben Lloyd <Ben.Lloyd@resonate.tech>
2025-06-12 16:08:24 +0100

    Add pagination to the object selector

--- src/app/features/room-uploader/room-uploader.component.ts
+++ src/app/features/room-uploader/room-uploader.component.ts
@@ -9,7 +9,9 @@ import {
   faImage, 
   faArrowRight, 
   faChevronDown, 
-  faChevronUp 
+  faChevronUp,
+  faChevronLeft,
+  faChevronRight
 } from '@fortawesome/free-solid-svg-icons';
 
 interface ObjectUpload {
@@ -36,6 +38,8 @@ export class RoomUploaderComponent {
   faArrowRight = faArrowRight;
   faChevronDown = faChevronDown;
   faChevronUp = faChevronUp;
+  faChevronLeft = faChevronLeft;
+  faChevronRight = faChevronRight;
 
   imageLabels = IMAGE_LABELS;
   objects: ObjectUpload[] = [{ 
@@ -47,7 +51,37 @@ export class RoomUploaderComponent {
   }];
   lastId = 0;
 
+  currentPage = 1;
+  itemsPerPage = 4;
+
   constructor(private router: Router) {}
+
+  get totalPages(): number {
+    return Math.ceil(this.objects.length / this.itemsPerPage);
+  }
+
+  get paginatedObjects(): ObjectUpload[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    return this.objects.slice(startIndex, startIndex + this.itemsPerPage);
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  goToPage(page: number): void {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+    }
+  }
 
   onFileSelected(event: Event, objectId: number, imageIndex: number): void {
     const input = event.target as HTMLInputElement;
@@ -66,7 +100,6 @@ export class RoomUploaderComponent {
     };
     reader.readAsDataURL(file);
   }
-
   addNewObject(): void {
     this.objects.forEach(obj => obj.isExpanded = false);
     
@@ -78,6 +111,11 @@ export class RoomUploaderComponent {
       thumbnails: [null, null, null, null],
       isExpanded: true 
     });
+
+    const newTotalPages = Math.ceil(this.objects.length / this.itemsPerPage);
+    if (newTotalPages > this.currentPage) {
+      this.currentPage = newTotalPages;
+    }
   }
 
   toggleObject(id: number): void {
@@ -86,9 +124,16 @@ export class RoomUploaderComponent {
       object.isExpanded = !object.isExpanded;
     }
   }
-
   removeObject(id: number): void {
+    const currentPageObjects = this.paginatedObjects;
+
     this.objects = this.objects.filter(o => o.id !== id);
+    
+    if (currentPageObjects.length === 1 && this.currentPage > 1) {
+      this.currentPage--;
+    } else if (this.currentPage > this.totalPages) {
+      this.currentPage = Math.max(1, this.totalPages);
+    }
   }
 
   removeImage(objectId: number, imageIndex: number): void {
