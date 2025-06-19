bf26e98c14bead006cebf2d1ea65019b63ff1207
Ben Lloyd <Ben.Lloyd@resonate.tech>
2025-06-19 13:41:27 +0100

    Add test config and update Netlify env var

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  BASE_API_URL: process.env['NG_APP_BASE_API_URL'] || "",
+  BASE_API_URL: process.env['NG_APP_BASE_API_URL'] || "http://127.0.0.1:5000",
 };
