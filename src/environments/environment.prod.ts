95bbdadaf880fcf0620c96208830221a64eddbc4
Ben Lloyd <Ben.Lloyd@resonate.tech>
2025-06-19 12:34:07 +0100

    Test env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  BASE_API_URL: 'https://dreamlab-backend-nov5.onrender.com'
+  BASE_API_URL: process.env['BASE_API_URL'] || 'https://dreamlab-backend-nov5.onrender.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 
 export const environment = {
-  BASE_API_URL: 'http://127.0.0.1:5000'
-};  
+  BASE_API_URL: process.env['BASE_API_URL'] || 'http://127.0.0.1:5000'
+};
